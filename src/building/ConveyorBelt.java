212e935d7a80ce98e095c6474472e6d33978da34
Natnaree Panya <umimchoi@gmail.com>
2019-10-26 19:23:53 +0700

    finish conveyorbelt

--- src/building/ConveyorBelt.java
+++ src/building/ConveyorBelt.java
@@ -13,22 +13,44 @@ import ui.BuildingIcon;
 import ui.ItemIcon;
 
 public class ConveyorBelt extends Building implements ItemReceiver, ItemProducer {	
+	private Direction direction;
+	private boolean beginn = false;
+	private Item onthisbelt=null;
 	public ConveyorBelt(Direction direction) {
-		
+		this.direction = direction;
 	}
 
 	
 	@Override
-	public void beforeCycle() {
-	}
-	
-	/* getters & setters */
-	public Item getItemOnBelt() {
-	}
-	
-	public Direction getDirection() {
+	public void operate() {
+		// TODO Auto-generated method stub
+		 Building back = Field.instance.getBuildingInBack(direction);
+		 Building front = Field.instance.getBuildingInFront(direction);
+		 if(this.canProduceItem()) {
+			 if(front instanceof ItemReceiver) {
+			if(((ItemReceiver) front).canReceiveItem(this.getItemOnBelt().getType())) {
+				((ItemReceiver) front).receiveItem(this.getItemOnBelt());
+			}
+			 }
+		 }
+		 else {
+			 if(back instanceof ItemProducer) {
+			 if(((ItemProducer) back).canProduceItem() && !beginn && this.onthisbelt == null){
+				 this.receiveItem(((ItemProducer) back).getProducedItem());
+			 }
+			 else {
+				 this.receiveItem(null);
+			 }
+			 }
+		 }
 	}
 
+
+	@Override
+	public void beforeCycle() {
+		this.beginn = false;
+	}
+	
 	@Override
 	public void render(StackPane target) {
 		
@@ -61,22 +83,11 @@ public class ConveyorBelt extends Building implements ItemReceiver, ItemProducer
 
 
 	@Override
-	public boolean canProduceItem() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	@Override
-	public Item getProducedItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
 	public boolean canReceiveItem(ItemType ofType) {
 		// TODO Auto-generated method stub
+		if(this.onthisbelt == null && !beginn && ofType != null) {
+			return true;
+		}
 		return false;
 	}
 
@@ -84,13 +95,38 @@ public class ConveyorBelt extends Building implements ItemReceiver, ItemProducer
 	@Override
 	public void receiveItem(Item item) {
 		// TODO Auto-generated method stub
+		beginn = true;
+		this.onthisbelt = item;
 		
 	}
 
 
 	@Override
-	public void operate() {
+	public boolean canProduceItem() {
 		// TODO Auto-generated method stub
-		
+		if(this.onthisbelt != null && !beginn) {
+			return true;
+		}
+		return false;
+	}
+
+
+	@Override
+	public Item getProducedItem() {
+		// TODO Auto-generated method stub
+		beginn = true;
+		Item send = this.onthisbelt;
+		this.onthisbelt = null;
+		return send;
+	}
+
+
+	/* getters & setters */
+	public Item getItemOnBelt() {
+		return this.onthisbelt;
+	}
+	
+	public Direction getDirection() {
+		return this.direction;
 	}
 }
